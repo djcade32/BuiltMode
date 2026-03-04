7a62f5a75d548aeeb848d879e5b1bc4bb121d91f
Norman Cade <norman_cade32@yahoo.com>
2026-03-04 05:03:48 -0500

    Use setUTCHours

--- functions/src/index.test.ts
+++ functions/src/index.test.ts
@@ -10,8 +10,8 @@ describe("handleGetWeekId", () => {
   test("returns previous Monday Feb 23, 2026 when date is Monday March 2, 2026 3:59 am", () => {
     const homeTimezone = "America/New_York";
     const date = new Date();
-    date.setUTCFullYear(2026, 2, 3);
-    date.setHours(3, 59, 0, 0);
+    date.setUTCFullYear(2026, 2, 2);
+    date.setUTCHours(8, 59, 0, 0);
 
     const dateStr = date.toISOString();
 
@@ -24,8 +24,8 @@ describe("handleGetWeekId", () => {
 
   test("returns March 2, 2026 when date is Monday March 2, 2026 4:00 am", () => {
     const date = new Date();
-    date.setUTCFullYear(2026, 2, 3);
-    date.setHours(4, 0, 0, 0);
+    date.setUTCFullYear(2026, 2, 2);
+    date.setUTCHours(9, 0, 0, 0);
 
     const res = handleGetWeekId(date, "America/New_York");
     expect(res).toEqual("2026-03-02");
@@ -58,8 +58,8 @@ describe("handleGetWeekId", () => {
   // Test timezone handling
   test("returns Monday March 02, 2026 in Tokyo when date is Monday March 2, 2026 3:59 am in New York ", () => {
     const date = new Date();
-    date.setUTCFullYear(2026, 2, 3);
-    date.setHours(3, 59, 0, 0);
+    date.setUTCFullYear(2026, 2, 2);
+    date.setUTCHours(8, 59, 0, 0);
 
     const nyTimeStr = dayjs(date).tz("America/New_York").toISOString();
 
@@ -68,23 +68,23 @@ describe("handleGetWeekId", () => {
   });
 
   // Test timezone handling
-  test("returns Monday Feb 23, 2026 in Tokyo Timezone when date is Monday March 2, 2026 1:59 pm in New York ", () => {
+  test("returns Monday March 2, 2026 in Tokyo Timezone when date is Sunday March 1, 2026 2:00 pm in New York ", () => {
     const date = new Date();
-    date.setUTCFullYear(2026, 2, 2);
-    date.setHours(13, 59, 0, 0);
+    date.setUTCFullYear(2026, 2, 1);
+    date.setUTCHours(19, 0, 0, 0);
 
     const nyTimeStr = dayjs(date).tz("America/New_York").toISOString();
 
     const res = handleGetWeekId(nyTimeStr, "Asia/Tokyo");
-    expect(res).toEqual("2026-02-23");
+    expect(res).toEqual("2026-03-02");
   });
 });
 
 describe("handleGetNextOfficialStartWeekId", () => {
   test("returns Monday March 2, 2026 when date is Monday March 2, 2026 3:59 am", () => {
     const date = new Date();
-    date.setUTCFullYear(2026, 2, 3);
-    date.setHours(3, 59, 0, 0);
+    date.setUTCFullYear(2026, 2, 2);
+    date.setUTCHours(8, 59, 0, 0);
 
     const res = handleGetNextOfficialStartWeekId(date, "America/New_York");
     expect(res).toEqual("2026-03-02");
@@ -92,8 +92,8 @@ describe("handleGetNextOfficialStartWeekId", () => {
 
   test("returns next Monday March 9, 2026 when date is Monday March 2, 2026 4:00 am", () => {
     const date = new Date();
-    date.setUTCFullYear(2026, 2, 3);
-    date.setHours(4, 0, 0, 0);
+    date.setUTCFullYear(2026, 2, 2);
+    date.setUTCHours(9, 0, 0, 0);
 
     const res = handleGetNextOfficialStartWeekId(date, "America/New_York");
     expect(res).toEqual("2026-03-09");
@@ -101,7 +101,7 @@ describe("handleGetNextOfficialStartWeekId", () => {
 
   test("returns next Monday March 9, 2026 when date is Wednesday March 4, 2026", () => {
     const date = new Date();
-    date.setUTCFullYear(2026, 2, 5);
+    date.setUTCFullYear(2026, 2, 4);
 
     const res = handleGetNextOfficialStartWeekId(date, "America/New_York");
     expect(res).toEqual("2026-03-09");
