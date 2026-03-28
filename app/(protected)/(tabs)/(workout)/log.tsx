7caa957bd649c654dca75971cf74039f68c9e71d
Norman Cade <norman_cade32@yahoo.com>
2026-03-28 07:54:39 -0400

    Built Log entry screen

---
+++ app/(protected)/(tabs)/(workout)/log.tsx
@@ -0,0 +1,159 @@
+import { ThemedText } from "@/components/themed-text";
+import ThemedButton from "@/components/ui/ThemedButton";
+import { Border, Colors, Typography } from "@/constants/theme";
+import { FontAwesome6, MaterialIcons } from "@expo/vector-icons";
+import { Link, useRouter } from "expo-router";
+import React from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const TEMPLATES = [
+  {
+    title: "Push Day",
+    numOfExercises: 5,
+  },
+  {
+    title: "Pull Day",
+    numOfExercises: 7,
+  },
+  {
+    title: "Shoulders Workout",
+    numOfExercises: 3,
+  },
+];
+
+type TemplateItemProps = {
+  icon: { iconFamily: any; name: string };
+  title: string;
+  numOfExercises: number;
+};
+
+const TemplateItem = ({ icon, title, numOfExercises }: TemplateItemProps) => {
+  // const renderIcon = React.createElement(icon.iconFamily, {
+  //   name: icon.name,
+  //   size: 17,
+  //   color: Colors.accent.primary,
+  // });
+  return (
+    <TouchableOpacity style={styles.templateItemContainer}>
+      {/* <View style={styles.templateItemIcon}>{renderIcon}</View> */}
+      <View
+        style={{
+          justifyContent: "space-between",
+          flexDirection: "row",
+          flex: 1,
+          alignItems: "center",
+        }}
+      >
+        <View>
+          <ThemedText style={styles.templateItemTitle}>{title}</ThemedText>
+          <ThemedText
+            style={styles.templateItemNumOfExercises}
+          >{`${numOfExercises} exercises`}</ThemedText>
+        </View>
+        <MaterialIcons name="keyboard-arrow-right" size={24} color={Colors.icon} />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const log = () => {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.titleContainer}>
+        <ThemedText type="title">LOG</ThemedText>
+        <ThemedText style={styles.subtitle}>Build your session. Then start.</ThemedText>
+      </View>
+      <ThemedButton
+        title="BUILD WORKOUT"
+        fontSize={Typography.size.sm}
+        onPress={() => router.push("/(protected)/(tabs)/(workout)/buildWorkout")}
+      />
+      <View style={styles.templatesSection}>
+        <ThemedText style={styles.templatesSectionTitle}>RECENT TEMPLATES</ThemedText>
+        <View style={styles.templatesContainer}>
+          {TEMPLATES.map((template, index) => (
+            <TemplateItem
+              key={index}
+              title={template.title}
+              numOfExercises={template.numOfExercises}
+              icon={{
+                iconFamily: FontAwesome6,
+                name: "dumbbell",
+              }}
+            />
+          ))}
+        </View>
+      </View>
+      <Link href={"/"} asChild style={{ marginTop: 48 }}>
+        <TouchableOpacity>
+          <ThemedText style={styles.viewHistoryButton}>VIEW HISTORY</ThemedText>
+        </TouchableOpacity>
+      </Link>
+    </SafeAreaView>
+  );
+};
+
+export default log;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 24,
+    paddingTop: 48,
+    backgroundColor: Colors.background.primary,
+    flex: 1,
+  },
+  titleContainer: {
+    gap: 8,
+    paddingBottom: 32,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: Colors.icon,
+    fontFamily: Typography.family.primary.medium,
+  },
+  templatesSection: {
+    paddingTop: 48,
+  },
+  templatesSectionTitle: {
+    fontSize: 12,
+    color: Colors.icon,
+    fontFamily: Typography.family.primary.semibold,
+  },
+  templatesContainer: {
+    paddingTop: 16,
+    gap: 12,
+  },
+  templateItemContainer: {
+    flexDirection: "row",
+    padding: 16,
+    backgroundColor: Colors.background.secondary,
+    borderRadius: Border.radius.md,
+    gap: 12,
+    alignItems: "center",
+  },
+  templateItemIcon: {
+    backgroundColor: "#c6a34a38",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 12,
+    borderRadius: Border.radius.md,
+  },
+  templateItemTitle: {
+    fontSize: 14,
+    fontFamily: Typography.family.primary.semibold,
+  },
+  templateItemNumOfExercises: {
+    fontSize: 12,
+    fontFamily: Typography.family.secondary.regular,
+    color: Colors.icon,
+  },
+  viewHistoryButton: {
+    fontFamily: Typography.family.primary.semibold,
+    fontSize: 14,
+    color: Colors.icon,
+    textAlign: "center",
+    letterSpacing: 0.7,
+  },
+});
